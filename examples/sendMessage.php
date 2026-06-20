003a2ffb51819494988495c91859133162ac8286
Sobhan Ghasemi <sobhansgh@gmail.com>
2026-06-20 20:47:14 +0330

    Create Rubika class

--- examples/sendMessage.php
+++ examples/sendMessage.php
@@ -1,1 +1,13 @@
 <?php
+require '../vendor/autoload.php';
+
+use RubikaBot\Rubika;
+
+$bot = new Rubika("TOKEN");
+
+$result = $bot->sendMessage([
+    "chat_id" => "...",
+    "text" => "سلام"
+]);
+
+print_r($result);
--- src/Rubika.php
+++ src/Rubika.php
@@ -1,1 +1,42 @@
 <?php
+
+namespace RubikaBot;
+
+class Rubika
+{
+    private string $token;
+
+    private string $baseUrl = "https://botapi.rubika.ir/v3";
+
+    public function __construct(string $token)
+    {
+        $this->token = $token;
+    }
+
+    private function request(string $method, array $params = [])
+    {
+        $url = $this->baseUrl . "/" . $this->token . "/" . $method;
+
+        $ch = curl_init($url);
+
+        curl_setopt_array($ch, [
+            CURLOPT_RETURNTRANSFER => true,
+            CURLOPT_POST => true,
+            CURLOPT_HTTPHEADER => [
+                "Content-Type: application/json"
+            ],
+            CURLOPT_POSTFIELDS => json_encode($params)
+        ]);
+
+        $response = curl_exec($ch);
+
+        curl_close($ch);
+
+        return json_decode($response, true);
+    }
+
+    public function sendMessage(array $params)
+    {
+        return $this->request("sendMessage", $params);
+    }
+}
